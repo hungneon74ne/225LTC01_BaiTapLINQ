3023064b72074e3646a160611161768e46155026
hungneon74ne <viethunghtht12345@gmail.com>
2026-03-14 20:09:53 +0700

    Phan2 LINQ voi Object

---
+++ Phan2/Bai10/Bai10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("Sinh viên: Lê Việt Hưng");
+        Console.WriteLine("MSV: 23115053122313");
+        Console.WriteLine("Lớp: 225LTC01");
+        Console.WriteLine("Bài 10: Sắp xếp theo điểm giảm dần\n");
+
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1, Name="An", Score=8},
+            new Student{Id=2, Name="Binh", Score=6},
+            new Student{Id=3, Name="Chi", Score=9},
+            new Student{Id=4, Name="Dung", Score=7}
+        };
+
+        var sorted = students.OrderByDescending(s => s.Score);
+
+        foreach (var s in sorted)
+        {
+            Console.WriteLine($"{s.Name} - {s.Score}");
+        }
+    }
+}
---
+++ Phan2/Bai11/Bai11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("Sinh viên: Lê Việt Hưng");
+        Console.WriteLine("MSV: 23115053122313");
+        Console.WriteLine("Lớp: 225LTC01");
+        Console.WriteLine("Bài 11: Sinh viên điểm cao nhất\n");
+
+        List<Student> students = new List<Student>()
+        {
+            new Student{Id=1, Name="An", Score=8},
+            new Student{Id=2, Name="Binh", Score=6},
+            new Student{Id=3, Name="Chi", Score=9},
+            new Student{Id=4, Name="Dung", Score=7}
+        };
+
+        var max = students.OrderByDescending(s => s.Score).First();
+
+        Console.WriteLine($"{max.Name} - {max.Score}");
+    }
+}
